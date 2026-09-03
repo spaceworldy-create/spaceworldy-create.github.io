289776070b6c4085f972903b9032de469350c054
spaceworldy-create <spaceworldy@gmail.com>
2026-09-03 13:32:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // เพิ่ม JavaScript สำหรับการโต้ตอบกับผู้ใช้ (optional)
  document.addEventListener('DOMContentLoaded',function () {
-     document.getELementById('year').innerHTML = new Date().getFullYaer();
-     console.Log('Portfolio ของ Niwat พร้อมแล้ว! ');
+     document.getElementById('year').innerHTML = new Date().getFullYaer();
+     console.log('Portfolio ของ Niwat พร้อมแล้ว! ');
 });
